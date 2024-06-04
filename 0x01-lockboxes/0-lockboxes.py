9c0d8422f998c456b309c08362cfdabdc9137d3e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-05 02:14:03 +0300

    c

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes) """
+
+
+def canUnlockAll(boxes):
+    """ Check if boxes can be unlocked """
+    for m in range(1, len(boxes) - 1):
+        r = False
+        for x in range(len(boxes)):
+            r = (m in boxes[idx] and m != x)
+            if r:
+                break
+        if r is False:
+            return r
+    return True
