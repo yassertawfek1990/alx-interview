cec1215c6d9dadcaf0d163f95f62da09c7f1fc21
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-19 19:58:26 +0300

    c

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
-""" Rs dsck"""
-import sys
+""" asd fd"""
 
 
-def print_msg(cs, f):
-    print("File size: <{}>".format(f))
-    for k, v in sorted(codes.items()):
-        if v != 0:
-            print("<{}>: <{}>".format(k, v))
+def parseLogs():
+    """
+    Readff
+    Reports: fdf
+    Raises: df"""
+    stdin = __import__('sys').stdin
+    ln = 0
+    fs = 0
+    st = {}
+    co = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for e in stdin:
+            ln += 1
+            e = e.split()
+            try:
+                fs += int(e[-1])
+                if e[-2] in co:
+                    try:
+                        st[e[-2]] += 1
+                    except KeyError:
+                        st[e[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if ln == 10:
+                report(fs, st)
+                ln = 0
+        report(fs, st)
+    except KeyboardInterrupt as e:
+        report(fs, st)
+        raise
 
 
-f = 0
-c = 0
-cl = 0
-cs = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+def report(f, sc):
+    """
+    Prrc
+    Args:
+        f (int): t
+        sc (dict): d
+    """
+    print("File size: {}".format(f))
+    for k, v in sorted(sc.items()):
+        print("{}: {}".format(k, v))
 
-try:
-    for las in sys.stdin:
-        pl = las.split()
-        pl = pl[::-1]
 
-        if len(pl) > 2:
-            cl += 1
-
-            if cl <= 10:
-                f += int(pl[0])
-                c = pl[1]
-
-                if (c in cs.keys()):
-                    cs[c] += 1
-
-            if (cl == 10):
-                print_msg(cs, f)
-                cl = 0
-
-finally:
-    print_msg(cs, f)
+if __name__ == '__main__':
+    parseLogs()
